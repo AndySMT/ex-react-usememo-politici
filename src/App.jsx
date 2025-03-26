0ee1ac3bc070741b4b776cacc627095d67a04d54
AndySMT <andyiulian.simota@gmail.com>
2025-03-26 10:34:53 +0100

    📌 Milestone 1: Recuperare e visualizzare i dati (Cards)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    async function fetchPoliticians() {
+      try {
+        const response = await fetch(
+          "https://boolean-spec-frontend.vercel.app/freetestapi/politicians"
+        );
+        if (!response.ok) {
+          throw new Error("Errore HTTP");
+        }
+        const data = await response.json();
+        setPoliticians(data);
+      } catch (error) {
+        console.error("Errore recupero politici:", error);
+      }
+    }
+    fetchPoliticians();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto bg-amber-400">
+        <h1 className="text-6xl italic font-bold flex justify-center align-middle mb-5">
+          Lista Politici
+        </h1>
+        <div className="flex flex-wrap justify-center gap-4">
+          {politicians.map((politician, id) => {
+            return (
+              <div
+                key={id}
+                className="max-w-sm bg-white rounded-lg overflow-hidden"
+              >
+                <div className="p-2 flex justify-center items-cente">
+                  <img
+                    src={politician.image}
+                    alt={politician.name}
+                    className="w-auto h-100"
+                  />
+                </div>
+                <div className="flex justify-between font-bold m-4">
+                  <p>{politician.name}</p>
+                  <p className="italic">{politician.position}</p>
+                </div>
+                <p className="p-1 m-2">{politician.biography}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
